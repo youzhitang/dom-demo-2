87e1b68b7a4a6c35498e238a19cf887ea71ede0b
tangyou <maoyou5749@163.com>
2020-11-02 18:23:35 +0800

    返回新的 api 对象,实现操作child

--- src/jquery.js
+++ src/jquery.js
@@ -1,15 +1,28 @@
-window.JQuery = function (selector) {
-    const elements = document.querySelectorAll(selector)
-    // api可以操作elements
-    return { //直接return一个对象
+window.jQuery = function (selectorOrArray) {
+    let elements
+    if (typeof selectorOrArray === 'string') {
+        elements = document.querySelectorAll(selectorOrArray)
+    } else if (selectorOrArray instanceof Array) {
+        //接收一个数组
+        elements = selectorOrArray
+    }
+    return {
         addClass: function (className) {
-            // 闭包：函数访问外部变量，addClass访问了elements
             for (let i = 0; i < elements.length; i++) {
                 elements[i].classList.add(className)
             }
             return this
-            //addClass里的this是api，这里已经不能return api了，因为已经没有api了，它的名字在外面
-        }
+        },
+        find(selector) {
+            let array = []
+            for (let i = 0; i < elements.length; i++) {
+                const elements2 = Array.from(elements[i].querySelectorAll(selector))
+                array = array.concat(elements2)
+            }
+            //const newApi = jQuery(array)
+            //靠jQuery构造出一个新API,如果每次用同一个API得到新的元素会污染之前的API
+            return jQuery(array)
+        },
     }
 
 }
